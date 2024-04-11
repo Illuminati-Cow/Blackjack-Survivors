fa6b0760128c8e5d782212e5e63003fdbc89b266
An-geles <66815800+An-geles@users.noreply.github.com>
2024-04-10 18:32:29 -0700

    update: movement know works but still needs to be improved

--- Player/Movement.gd
+++ Player/Movement.gd
@@ -1,11 +1,21 @@
-extends Node
+extends CharacterBody2D
 
+const GRAVITY = 200.0
+const WALK_SPEED = 200
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta):
 
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= WALK_SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y += WALK_SPEED
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x =  WALK_SPEED
+	else:
+		velocity.x = 0
+		velocity.y = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	# "move_and_slide" already takes delta time into account.
+	move_and_slide()
