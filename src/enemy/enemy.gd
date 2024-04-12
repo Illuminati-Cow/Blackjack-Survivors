8cd54bd93f503b4f642ee5c73f58110ce6482e51
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-04-12 12:56:31 -0700

    Steering Manager Debug Gizmos and Made Slowdown on Approach Optional

--- src/enemy/enemy.gd
+++ src/enemy/enemy.gd
@@ -1,10 +1,15 @@
 class_name Enemy extends CharacterBody2D
 
 # Public properties
+@export_category("Movement")
 @export var max_speed : float
 @export var max_force : float
 @export var mass : float
-
+@export_category("Debug")
+@export var debug_mode : bool = false
+# TESTING: REMOVE THIS
+@export var target : CharacterBody2D = null
+var default_font : Font = ThemeDB.fallback_font;
 # Components
 @onready
 var _health_component : Health = $Health
@@ -17,5 +22,18 @@ var _steering_manager : Steering_Manager = Steering_Manager.new(
 )
 
 func _physics_process(delta):
-	_steering_manager.seek(get_global_mouse_position())
-	_steering_manager.move(delta)
+	if target != null:
+		_steering_manager.pursue(target)
+	var collisions = _steering_manager.move(delta)
+	if debug_mode:
+		queue_redraw()
+
+func _draw():
+	if not debug_mode:
+		return
+	var speed = str(floor(velocity.length()))
+	draw_string(default_font, Vector2(20, -10), speed,
+				HORIZONTAL_ALIGNMENT_CENTER, 40, 18)
+	if target != null:
+		draw_string(default_font, Vector2(20, -10), speed,
+				HORIZONTAL_ALIGNMENT_CENTER, 40, 18)
