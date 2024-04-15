b5c1bd8685a990c80eebf9aed199d113448cd0d0
An-geles <66815800+An-geles@users.noreply.github.com>
2024-04-15 15:38:53 -0700

    update: now uses 8 directional movement with uniform speed

--- Player/Movement.gd
+++ Player/Movement.gd
@@ -1,21 +1,11 @@
 extends CharacterBody2D
 
-const GRAVITY = 200.0
-const WALK_SPEED = 200
+@export var speed = 500
+
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_direction * speed
 
 func _physics_process(delta):
-
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= WALK_SPEED
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y += WALK_SPEED
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -WALK_SPEED
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x =  WALK_SPEED
-	else:
-		velocity.x = 0
-		velocity.y = 0
-
-	# "move_and_slide" already takes delta time into account.
+	get_input()
 	move_and_slide()
