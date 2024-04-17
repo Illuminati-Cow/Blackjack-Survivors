6e62429ea996aaf9044c5854f7a8c5cfbb7567c2
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-04-16 20:39:43 -0700

    Fixed card enemies and their spawning

--- src/game_manager/game_manager.gd
+++ src/game_manager/game_manager.gd
@@ -3,7 +3,7 @@ extends Node
 @onready
 var root = get_tree().current_scene
 @onready
-var blackjack_manager = root.get_node("BlackjackManager").get_script() as Blackjack
+var blackjack_manager = root.get_node("BlackjackManager") as Blackjack
 # WARNING: This is temporary, it likely needs to be refactored so that types are
 # obvious and scripts are easily accesible. Also needs to use weighted random.
 @onready
@@ -12,11 +12,12 @@ var card_enemy = load("res://src/enemy/card_enemy.res")
 var enemies = [
 	card_enemy
 ]
-@onready var player = root.get_node("PlayerTest")
+@onready var player = root.get_node("Player")
 
 # Spawn a random enemy
 func _on_spawn_timer_timeout():
-	var enemy := enemies.pick_random().instantiate() as Enemy
+	var enemy := enemies.pick_random().instantiate() as CardEnemy
+	enemy.set_card(blackjack_manager.draw())
 	# WARNING: Change this to spawn around player
 	enemy.position = Vector2(randi_range(-5000, 5000), randi_range(-5000, 5000))
 	enemy.target = player
