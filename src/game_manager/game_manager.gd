7202323d0f77a666a6ef2b69535b9f0189f7e643
Cal Friedman School <cffriedm@ucsc.edu>
2024-04-16 21:03:54 -0700

    new changes from main part 1

--- src/game_manager/game_manager.gd
+++ src/game_manager/game_manager.gd
@@ -3,7 +3,7 @@ extends Node
 @onready
 var root = get_tree().current_scene
 @onready
-var blackjack_manager = root.get_node("BlackjackManager").get_script() as Blackjack
+var blackjack_manager = root.get_node("BlackjackManager") as Blackjack
 # WARNING: This is temporary, it likely needs to be refactored so that types are
 # obvious and scripts are easily accesible. Also needs to use weighted random.
 @onready
@@ -16,7 +16,8 @@ var enemies = [
 
 # Spawn a random enemy
 func _on_spawn_timer_timeout():
-	var enemy := enemies.pick_random().instantiate() as Enemy
+	var enemy := enemies.pick_random().instantiate() as CardEnemy
+	enemy.set_card(blackjack_manager.draw())
 	# WARNING: Change this to spawn around player
 	enemy.position = Vector2(randi_range(-5000, 5000), randi_range(-5000, 5000))
 	enemy.target = player
