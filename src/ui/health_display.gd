1cdf1c8b98a7a6e994c3a8a1007a1d7fad7acdb2
Cole Falxa-Sturken <colefalxa@gmail.com>
2024-04-18 16:56:23 -0700

    Health added

---
+++ src/ui/health_display.gd
@@ -0,0 +1,20 @@
+extends TextureRect
+
+var _tx_size : Vector2i
+
+func _on_health_change(new_health : int):
+	assert(new_health >= 0 and new_health <= 13)
+	_update_health_card(new_health)
+
+
+func _update_health_card(health : int):
+	if health == 0:
+		texture.region.size = Vector2i.ZERO
+		return
+	else:
+		texture.region.size = _tx_size
+		
+	var txr := texture as AtlasTexture
+	var x = txr.region.size.x * (health - 1)
+	var y = txr.region.size.y
+	txr.region = Rect2i(Vector2i(x, y), txr.region.size)
--- src/ui/ui_hand.gd
+++ src/ui/ui_hand.gd
@@ -4,6 +4,8 @@ extends Control
 
 @onready
 var card_spritesheet : Texture2D = preload("res://assets/cards_spritesheet.png")
+@export
+var texture_region_size : Vector2i = Vector2i(65, 80)
 @export_range(0, 30)
 var card_spread_angle : float = 15
 @export_range(0.5, 3)
@@ -15,33 +17,38 @@ var card_scale : float = 1.5
 @export var fall_scale : float = 1.5
 @export_group("Card Shift Tween")
 @export var shift_time : float = 0.25
-var tween_lock := false
 
+var _tween_lock := false
 var _card_queue := []
 var _cards := []
+var _original_size : Vector2
+
+func _ready():
+	_original_size = size
+
 
 func _process(delta):
 	if not _card_queue.is_empty():
-		if not tween_lock:
-			var card := _card_queue.pop_front() as Control
+		if not _tween_lock:
+			var card := _card_queue.pop_front() as TextureRect
 			_play_card_fall_tween(card)
-			tween_lock = true
+			_tween_lock = true
 			if not _cards.is_empty():
 				var tween = _cards.front().create_tween().set_parallel()
-				for c in _cards:
+				for c : TextureRect in _cards:
 					var s = min(shift_time + 0.05 * _cards.find(c), fall_time)
 					c.z_index -= 1
-					var end_rot = c.rotation - deg_to_rad(card_spread_angle)
-					tween.tween_property(c, "rotation", end_rot, s)\
+					var end_rot = c.rotation_degrees - card_spread_angle
+					tween.tween_property(c, "rotation_degrees", end_rot, s)\
 						.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
 			_cards.append(card)
 	
 	# Fix for new hand signal spam permanently locking tween_lock
-	if _cards.is_empty() and tween_lock:
-		tween_lock = false
+	if _cards.is_empty() and _tween_lock:
+		_tween_lock = false
 
 
-func _play_card_fall_tween(card : Control):
+func _play_card_fall_tween(card : TextureRect):
 	card.scale = Vector2(fall_scale, fall_scale)
 	card.visible = true
 	card.position = spawn_location + fall_drift
@@ -50,39 +57,39 @@ func _play_card_fall_tween(card : Control):
 		.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
 	tween.tween_property(card, "scale", Vector2(card_scale, card_scale), fall_time)\
 		.set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
-	tween.chain().tween_callback(
-			func(): 
-				tween_lock = false
-				print("Unlocked")
-	)
+	tween.chain().tween_callback(func(): _tween_lock = false)
 
 
 func suit_to_int(suit : Blackjack.Card.Suit) -> int:
 	return Blackjack.Card.Suit.values()[suit]
 
 
-func get_card_sprite(card : Blackjack.Card) -> Sprite2D:
+func get_card_texture(card : Blackjack.Card) -> AtlasTexture:
 	var suit := suit_to_int(card.suit)
 	var rank := card.rank
-	var card_sprite := Sprite2D.new()
-	card_sprite.texture = card_spritesheet
-	card_sprite.hframes = 13
-	card_sprite.vframes = 5
-	card_sprite.frame = suit * 13 + rank - 1
-	return card_sprite
+	var texture := AtlasTexture.new()
+	texture.atlas = card_spritesheet
+	var x = texture_region_size.x * (rank - 1)
+	var y = texture_region_size.y * suit
+	texture.region = Rect2i(Vector2i(x,y), texture_region_size)
+	return texture
 
-func create_card(sprite : Sprite2D) -> Control:
-	var control = Control.new()
-	add_child(control)
-	control.add_child(sprite)
-	control.pivot_offset = card_scale * Vector2(0, 80)
-	control.visible = false
-	return control
+
+func create_card(texture : Texture2D) -> TextureRect:
+	var t_r = TextureRect.new()
+	t_r.texture = texture
+	add_child(t_r)
+	t_r.pivot_offset = Vector2(0, card_scale * texture_region_size.y)
+	t_r.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
+	t_r.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	t_r.custom_minimum_size = texture_region_size
+	t_r.visible = false
+	return t_r
 
 
 func _on_blackjack_manager_house_hit(card):
-	print(card.rank)
-	_card_queue.append(create_card(get_card_sprite(card)))
+	print_debug(card.rank)
+	_card_queue.append(create_card(get_card_texture(card)))
 
 
 func _on_blackjack_manager_hands_cleared():
