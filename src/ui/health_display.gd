ef25d54b9c73b269c1dcce4ae27042e542a8d46d
An-geles <66815800+An-geles@users.noreply.github.com>
2024-04-19 12:33:56 -0700

    update: fixing crash bug

--- src/ui/health_display.gd
+++ src/ui/health_display.gd
@@ -7,14 +7,25 @@ func _on_health_change(new_health : int):
 	_update_health_card(new_health)
 
 
+#func _update_health_card(health : int):
+	#var txr := texture as AtlasTexture
+	#if health == 0:
+		#txr.region.size = Vector2i.ZERO
+		#return
+	#else:
+		#txr.region.size = _tx_size
+		#
+	#var x = txr.region.size.x * (health - 1)
+	#var y = txr.region.size.y
+	#txr.region = Rect2i(Vector2i(x, y), txr.region.size)
+
+
 func _update_health_card(health : int):
 	var txr := texture as AtlasTexture
 	if health == 0:
-		txr.region.size = Vector2i.ZERO
+		txr.region = Rect2i(Vector2i.ZERO, Vector2i.ZERO)
 		return
 	else:
-		txr.region.size = _tx_size
-		
-	var x = txr.region.size.x * (health - 1)
-	var y = txr.region.size.y
-	txr.region = Rect2i(Vector2i(x, y), txr.region.size)
+		var x = _tx_size.x * (health - 1)
+		var y = 0  # Assuming y starts at 0, adjust if necessary
+		txr.region = Rect2i(Vector2i(x, y), _tx_size)
